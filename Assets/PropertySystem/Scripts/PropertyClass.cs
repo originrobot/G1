4affb44c9165ae8ec2618f68b2c803b53a2159d9
Raven <raven@poopies.cn>
2015-02-27 16:08:12 +0800

    added PropertyClass

---
+++ Assets/PropertySystem/Scripts/PropertyClass.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+public class PropertyClass
+{
+	private string _className;
+	public string className
+	{
+		get { return _className; }
+		set { _className = value; }
+	}
+	private Dictionary<string, PropertyBase> _properties = new Dictionary<string, PropertyBase>();
+
+	public void addProperty(PropertyBase property)
+	{
+		if (!_properties.ContainsKey(property.name))
+		{
+			_properties.Add(property.name, property);
+		}
+	}
+}
